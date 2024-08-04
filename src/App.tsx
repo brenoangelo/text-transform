ade5e4876a022d56ae7d9a9b00d77fc66b808067
Breno Angelo <brenoangeloal@gmail.com>
2024-08-03 23:02:20 -0300

    feat: add meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import { TextTransform } from './components/TextTransform';
 import { GlobalStyle } from './styles/global';
 import { darkTheme } from './styles/themes/dark';
 import { AdsenseComponent } from './adsense';
+import { Helmet } from 'react-helmet';
 
 export function App() {
   return (
     <ThemeProvider theme={darkTheme}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>
+          Text Transform
+        </title>
+        <meta name="description" content="Transforme seus textos para o que você precisar!" />
+        <link rel="canonical" href="http://textransform.com.br" />
+      </Helmet>
       <Header />
       <TextTransform />
 
